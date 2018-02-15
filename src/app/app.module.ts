489c5da46189b6f99e917ebb9617a76c8e3a1023
Paulo Forestieri <paulofore@gmail.com>
2018-02-15 20:38:11 -0200

    React Form - Parte 1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -23,6 +23,7 @@ import { AppRoutingModule } from './app-routing/app-routing.module';
 import { LoginComponent } from './login/login.component';
 
 import { FormsModule } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -42,7 +43,8 @@ import { FormsModule } from '@angular/forms';
     MaterialModule,
     FlexLayoutModule,
     AppRoutingModule,
-    FormsModule
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [
     DishService,
--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Feedback, ContactType } from '../shared/feedback';
+
 @Component({
   selector: 'app-contact',
   templateUrl: './contact.component.html',
@@ -7,9 +10,33 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ContactComponent implements OnInit {
 
-  constructor() { }
+ 
+  feedbackFormGroup: FormGroup;  
+  feedback: Feedback;
+  listaTipo = ContactType;
+
+  constructor(private fb: FormBuilder) { 
+    this.createForm(); 
+  }
 
   ngOnInit() {
   }
 
+  createForm() {
+    this.feedbackFormGroup = this.fb.group({
+        firstname: '',
+        lastname: '',
+        telnum: 0,
+        email: '',
+        agree: false,
+        contact: 'Nenhum',
+        message: ''
+    })
+  }
+
+  onSubmit() {
+    this.feedback = this.feedbackFormGroup.value;
+    console.log(this.feedback);
+    this.feedbackFormGroup.reset();
+  }
 }
