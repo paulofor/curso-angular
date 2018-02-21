715d5b7d0b02d35d45b865387c77ed2393744e98
Paulo <paulofore@gmail.com>
2018-02-21 20:03:48 -0300

    Rect form - Parte 3

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -11,9 +11,36 @@ import { Feedback, ContactType } from '../shared/feedback';
 export class ContactComponent implements OnInit {
 
  
+  
   feedbackFormGroup: FormGroup;  
   feedback: Feedback;
   listaTipo = ContactType;
+  formErros = {
+    'firstname': '',
+    'lastname': '',
+    'telnum': '',
+    'email' : ''
+  }
+  validacaoMensagens = {
+    'firstname' : {
+      'required' : 'Primeiro nome obrigatorio',
+      'minlength' : 'Campo não pode ser inferior a 2 caracteres',
+      'maxlength' : 'Campo não pode ser superior a 25 caracteres'
+    }, 
+    'lastname' : {
+      'required' : 'Ultimo nome obrigatorio',
+      'minlength' : 'Campo não pode ser inferior a 2 caracteres',
+      'maxlength' : 'Campo não pode ser superior a 25 caracteres'
+    }, 
+    'telnum' : {
+      'required' : 'Telefone obrigatorio',
+      'pattern' : 'Telefone invalido',
+    },
+    'email' : {
+      'required' : 'Email obrigatorio',
+      'email' : 'Email invalido',
+    }
+  }
 
   constructor(private fb: FormBuilder) { 
     this.createForm(); 
@@ -24,15 +51,37 @@ export class ContactComponent implements OnInit {
 
   createForm() {
     this.feedbackFormGroup = this.fb.group({
-        firstname: ['', Validators.required ],
-        lastname: ['', Validators.required ],
-        telnum: [0, Validators.required ],
-        email: ['', Validators.required ],
+        firstname: ['', [Validators.required, Validators.minLength(2), Validators.maxLength(25) ]],
+        lastname: ['', [Validators.required,Validators.minLength(2), Validators.maxLength(25) ]],
+        telnum: [0, [Validators.required, Validators.pattern  ] ],
+        email: ['', [Validators.required, Validators.email ]  ],
         agree: false,
         contact: 'Nenhum',
         message: ''
     })
+
+    this.feedbackFormGroup.valueChanges
+      .subscribe(data => this.onValueChanged(data) );
+    
+    this.onValueChanged();  // reset
   }
+
+  onValueChanged(data?:any) {
+    if (!this.feedbackFormGroup) return;
+    const form = this.feedbackFormGroup;
+
+    for (const campo in this.formErros) {
+        this.formErros[campo] = '';
+        const controle = form.get(campo);
+        if (controle && controle.dirty && !controle.valid) {
+          const mensagem = this.validacaoMensagens[campo];
+          for (const key in controle.errors) {
+            this.formErros[campo] += mensagem[key] + ' ';
+          }
+        }
+    }
+  }
+
 
   onSubmit() {
     this.feedback = this.feedbackFormGroup.value;
